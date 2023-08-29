210919b826168364e2b1dc51c9d870bf12ad8bde
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-08-29 14:40:14 +0600

    c

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-var x = document.querySelector(".p1");
-var y = document.querySelector(".p2");
-function addStyle() {
-  x.classList.add("para-style");
-  y.classList.add("para-style2");
-}
-function removeStyle() {
-  x.classList.remove("para-style");
-  y.classList.remove("para-style2");
-}
+document.querySelector("button").addEventListener("click", function () {
+  alert("hello");
+});
