95e5018eae291c99acc95d1c76c54a608d67f6a4
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-08-29 14:46:56 +0600

    c

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.querySelector("button").addEventListener("click", function () {
-  alert("hello");
+var myVar = document.querySelector("h1");
+
+myVar.addEventListener("mouseover", function () {
+  myVar.classList.add("h1-style");
+});
+myVar.addEventListener("mouseout", function () {
+  myVar.classList.remove("h1-style");
 });
