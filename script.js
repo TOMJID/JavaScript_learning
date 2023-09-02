973c748e6d120fa540383eeb9cb87f8ae1ecaae1
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-09-02 12:45:21 +0600

    add and remove animation

--- script.js
+++ script.js
@@ -6,21 +6,30 @@ for (var x = 0; x < lan; x++) {
       var text = this.innerHTML;
       console.log(text);
       audioPlay(text);
+      playAnimation(text);
     });
 }
 function audioPlay(text) {
   switch (text) {
-    case "Button 1":
+    case "a":
       var audio = new Audio("Sounds/1.mp3");
       audio.play();
       break;
-    case "Button 2":
+    case "b":
       var audio = new Audio("Sounds/2.mp3");
       audio.play();
       break;
-    case "Button 3":
+    case "c":
       var audio = new Audio("Sounds/3.mp3");
       audio.play();
       break;
   }
+}
+function playAnimation(text) {
+  var selectedButton = document.querySelector("." + text);
+  selectedButton.classList.add("anim");
+
+  setTimeout(function () {
+    selectedButton.classList.remove("anim");
+  }, 1000);
 }
