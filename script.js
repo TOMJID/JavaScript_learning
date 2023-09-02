ae7c4773a6ee57b940fd3f5bd0c32e98f22500b9
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-09-02 12:14:03 +0600

    play audio in javascript v2

--- script.js
+++ script.js
@@ -5,19 +5,22 @@ for (var x = 0; x < lan; x++) {
     [x].addEventListener("click", function () {
       var text = this.innerHTML;
       console.log(text);
-      switch (text) {
-        case "Button 1":
-          var audio = new Audio("Sounds/1.mp3");
-          audio.play();
-          break;
-        case "Button 2":
-          var audio = new Audio("Sounds/2.mp3");
-          audio.play();
-          break;
-        case "Button 3":
-          var audio = new Audio("Sounds/3.mp3");
-          audio.play();
-          break;
-      }
+      audioPlay(text);
     });
+}
+function audioPlay(text) {
+  switch (text) {
+    case "Button 1":
+      var audio = new Audio("Sounds/1.mp3");
+      audio.play();
+      break;
+    case "Button 2":
+      var audio = new Audio("Sounds/2.mp3");
+      audio.play();
+      break;
+    case "Button 3":
+      var audio = new Audio("Sounds/3.mp3");
+      audio.play();
+      break;
+  }
 }
