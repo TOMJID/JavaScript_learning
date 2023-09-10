99f1df48c664f7ae1b7c9bb989a1bd9399891b3d
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-09-10 12:37:33 +0600

    add litener with chackbox

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-var input = document.querySelector("input[name=name]");
-input.addEventListener("change", changeHandler);
-function changeHandler(e) {
-  // console.log(e);
-  // console.log(e.type);
-  // console.log(e.target);
-  // console.log(e.target.className);
-  // console.log(e.target.id);
-  console.log(e.target.value);
+//for input
+
+// var input = document.querySelector("input[name=name]");
+// input.addEventListener("change", changeHandler);
+// function changeHandler(e) {
+//   // console.log(e);
+//   // console.log(e.type);
+//   // console.log(e.target);
+//   // console.log(e.target.className);
+//   // console.log(e.target.id);
+//   console.log(e.target.value);
+// }
+
+//add litener with chackbox
+const programs = document.querySelectorAll("input[name=program]");
+// console.log(programs);
+
+Array.from(programs).map((program) => {
+  program.addEventListener("change", programHandler);
+});
+
+function programHandler(e) {
+  if (e.target.checked) {
+    console.log(e.target.value);
+  }
 }
