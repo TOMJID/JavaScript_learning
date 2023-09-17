a89dfe59232bf516a2ccab1ef14469f27314a480
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-09-17 12:38:19 +0600

    Images next button

---
+++ auto css adding/script.js
@@ -0,0 +1,10 @@
+var x = document.querySelector(".p1");
+var y = document.querySelector(".p2");
+function addStyle() {
+  x.classList.add("para-style");
+  y.classList.add("para-style2");
+}
+function removeStyle() {
+  x.classList.remove("para-style");
+  y.classList.remove("para-style2");
+}
--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-var video = document.querySelector("video");
-video.addEventListener("canplay", function () {
-  console.log("canplay");
-});
-video.addEventListener("play", function () {
-  console.log("play");
-});
-video.addEventListener("playing", function () {
-  console.log("playing");
-});
-video.addEventListener("pause", function () {
-  console.log("pause");
-});
-video.addEventListener("ended", function () {
-  console.log("thanks for watching");
-});
-video.addEventListener("volumechange", function () {
-  console.log("volumechange");
-});
+var photos = [
+  "images/pic1.jpg",
+  "images/pic2.jpg",
+  "images/pic3.jpg",
+  "images/pic4.jpg",
+];
+var imgTag = document.querySelector("img");
+var count = 0;
+function next() {
+  count++;
+  if (count >= photos.length) {
+    count = 0;
+    imgTag.src = photos[count];
+  } else {
+    imgTag.src = photos[count];
+  }
+}
+function prev() {
+  count--;
+  if (count < 0) {
+    count = photos.length - 1;
+    imgTag.src = photos[count];
+  } else {
+    imgTag.src = photos[count];
+  }
+}
