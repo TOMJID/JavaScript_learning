0d819d4f8bba40caa0892450d05417a886f662f0
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-09-17 12:39:27 +0600

    Event Listeners with multiple elements

--- Event Listeners with multiple elements/script.js
+++
@@ -1,9 +0,0 @@
-var lan = document.querySelectorAll(".myButton").length;
-for (var x = 0; x < lan; x++) {
-  document
-    .querySelectorAll(".myButton")
-    [x].addEventListener("click", function () {
-      var text = this.innerHTML;
-      document.querySelector("h1").innerHTML = text + "is clicked";
-    });
-}
--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-var photos = [
-  "images/pic1.jpg",
-  "images/pic2.jpg",
-  "images/pic3.jpg",
-  "images/pic4.jpg",
-];
-var imgTag = document.querySelector("img");
-var count = 0;
-function next() {
-  count++;
-  if (count >= photos.length) {
-    count = 0;
-    imgTag.src = photos[count];
-  } else {
-    imgTag.src = photos[count];
-  }
-}
-function prev() {
-  count--;
-  if (count < 0) {
-    count = photos.length - 1;
-    imgTag.src = photos[count];
-  } else {
-    imgTag.src = photos[count];
-  }
+var lan = document.querySelectorAll(".myButton").length;
+for (var x = 0; x < lan; x++) {
+  document
+    .querySelectorAll(".myButton")
+    [x].addEventListener("click", function () {
+      var text = this.innerHTML;
+      document.querySelector("h1").innerHTML = text + "is clicked";
+    });
 }
