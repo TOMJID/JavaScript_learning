53c1a685afa69e055d0237290513b9affeeea469
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-09-17 12:37:26 +0600

    how to handle video/media events

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-const form = document.querySelector("form");
-const name = form.querySelector("div #name");
-// console.log(Name);
-const email = form.querySelector("div #email");
-// console.log(email);
-const password = form.querySelector("div #password");
-// console.log(password);
-
-form.addEventListener("submit", formSubmit);
-
-function formSubmit(e) {
-  e.preventDefault();
-  var userInfo = {
-    name: name.value,
-    email: email.value,
-    password: password.value,
-  };
-  console.log(userInfo);
-  //after submiting at will make the form empty
-  name.value = "";
-  email.value = "";
-  password.value = "";
-}
+var video = document.querySelector("video");
+video.addEventListener("canplay", function () {
+  console.log("canplay");
+});
+video.addEventListener("play", function () {
+  console.log("play");
+});
+video.addEventListener("playing", function () {
+  console.log("playing");
+});
+video.addEventListener("pause", function () {
+  console.log("pause");
+});
+video.addEventListener("ended", function () {
+  console.log("thanks for watching");
+});
+video.addEventListener("volumechange", function () {
+  console.log("volumechange");
+});
