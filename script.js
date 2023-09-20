609bc416b1ed800fb0dee0b8cc9c5a5f351b4c4e
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-09-20 15:53:04 +0600

    DOM Event | KeyboardEvent

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-var btn = document.querySelectorAll(".btn");
+//keybared Event object
+//1.Keydown - pressing a key, can repeat
+//2.keypress (may not supported by some borwsers)
+//3.keyup
 
-Array.from(btn).map((button) => {
-  button.addEventListener("click", function (e) {
-    console.log(e.target.innerText);
-  });
+//some properties = key,keyCode,code,shiftKey,ctrlKey,prpeat
+
+var textarea = document.querySelector("textarea");
+
+//1
+textarea.addEventListener("keydown", function (e) {
+  console.log("keydown");
+  if (e.repeat) {
+    alert("do not repeat");
+  }
 });
+
+//2
+// textarea.addEventListener("keypress", function () {
+//   console.log("keypress");
+// });
+
+//3
+// textarea.addEventListener("keyup", function (e) {
+//   // console.log(e.key);
+//   // console.log(e.keyCode);
+//   // console.log(e.code);
+//   // console.log(e.shiftKey);
+//   if (e.shiftKey) {
+//     console.log("shift +" + e.key);
+//   }
+// });
