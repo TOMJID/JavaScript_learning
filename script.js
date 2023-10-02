f1a3705ec234c0da599db7bba4c6dfb6914d807a
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-10-02 11:53:45 +0600

    Browser Object Model | Popup boxes
    prompt box (where user will enter ther name and it will show it in HTML

--- script.js
+++ script.js
@@ -5,12 +5,28 @@
 
 // confirm("are you sure")
 
-function deleteSomething() {
-  let value = confirm("Do you want to delete this ?");
-  if (value) {
-    console.log("deleted");
+// function deleteSomething() {
+//   let value = confirm("Do you want to delete this ?");
+//   if (value) {
+//     console.log("deleted");
+//   } else {
+//     console.log("not deleted");
+//   }
+// }
+// deleteSomething()
+
+function welcomeMassage() {
+  var welcome = document.createElement("h2");
+  var text;
+  var userName = prompt("enter your name");
+
+  if (userName == null || userName == "") {
+    text = "no name found";
   } else {
-    console.log("not deleted");
+    text = userName;
   }
+  var textNode = document.createTextNode(text);
+  welcome.appendChild(textNode);
+  document.body.appendChild(welcome);
 }
-deleteSomething()
+welcomeMassage();
