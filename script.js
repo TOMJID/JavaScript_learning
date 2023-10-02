fa9c39fe8b82a140b74f779f6cbb3066ea683212
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-10-02 11:39:29 +0600

    Browser Object Model  (BOM)
    alert box and confirm box

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-//BOM {Browser Object Model}
-//1.window object
-//2.location object
+//JS BOM
+//POPUP BOXES - alert , confirm , prompt
 
-console.clear();
+// alert("error here")
 
-var locationDiv = document.querySelector(".location-div");
-console.log(locationDiv);
+// confirm("are you sure")
 
-var p1 = locationDiv.children[0];
-p1.textContent = location.href;
-
-var p2 = locationDiv.children[1];
-p2.textContent = location.hostname;
-
-var p3 = locationDiv.children[2];
-p3.textContent = location.protocol;
-
-var p4 = locationDiv.children[3];
-p4.textContent = location.port;
-
-var p5 = locationDiv.children[4];
-p5.textContent = location.pathname;
-
-
-
-
-var button = document.querySelector("#visit-button");
-button.addEventListener("click", function () {
-  location.assign("https://www.google.com");
-});
+function deleteSomething() {
+  let value = confirm("Do you want to delete this ?");
+  if (value) {
+    console.log("deleted");
+  } else {
+    console.log("not deleted");
+  }
+}
+deleteSomething()
