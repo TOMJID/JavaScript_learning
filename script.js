51d9ddbb79aae74696f0458c44e024957dc1853b
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-10-04 11:51:06 +0600

    how to handle error in JavaScript
    Part 1

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-var saveBtn = document.querySelector(".save-button");
-var message = document.querySelector(".messages");
-
-saveBtn.addEventListener("click", startClock);
-
-function startClock() {
-  var date = new Date();
-
-  let seconds = date.getSeconds();
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-
-  seconds = formetTime(seconds);
-  minutes = formetTime(minutes);
-  var time = hours + ":" + minutes + ":" + seconds;
-  message.textContent = time;
-  setInterval(startClock, 1000);
-}
-
-function formetTime(value) {
-  if (value < 10) {
-    value = "0" + value;
-  }
-  return value;
+try {
+  alert("hi everyone");
+  alert(x);
+  alert("bye everyonne");
+} catch (err) {
+  console.log(err);
+} finally {
+  alert("bye everyone");
 }
