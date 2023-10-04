5b0cea1659bcfaa5d1a298731b1c33f29f9ce7d1
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-10-04 12:01:07 +0600

    how to handle error in JavaScript
    part 2

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-try {
-  alert("hi everyone");
-  alert(x);
-  alert("bye everyonne");
-} catch (err) {
-  console.log(err);
-} finally {
-  alert("bye everyone");
-}
+document.querySelector("#chackBtn").addEventListener("click", function () {
+  var num = document.querySelector("#numTextField").value;
+
+  try {
+    if (num < 5) {
+      throw "input is to low";
+    } else if (num > 10) {
+      throw "input is too high";
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
