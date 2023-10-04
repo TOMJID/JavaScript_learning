1c47b68a648118a75c67bc7e2e369507ab9be824
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-10-04 12:24:28 +0600

    Canvas
    drawing graphics on web page

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-document.querySelector("#chackBtn").addEventListener("click", function () {
-  var num = document.querySelector("#numTextField").value;
+var c = document.querySelector("#myCanvas");
 
-  try {
-    if (num < 5) {
-      throw "input is to low";
-    } else if (num > 10) {
-      throw "input is too high";
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
+var ctx = c.getContext("2d");
+
+ctx.lineWidth = 3;
+ctx.strokeStyle = "white";
+ctx.strokeRect(10, 10, 360, 210);
+
+ctx.fillStyle = "green";
+ctx.fillRect(12, 12, 358, 208);
+
+var centerX = 358 / 2;
+var centerY = 208 / 2;
+
+ctx.beginPath();
+ctx.arc(centerX, centerY, 50, 0, 2 * Math.PI, false);
+ctx.fillStyle = "red";
+ctx.lineWidth = 1;
+ctx.fill();
+ctx.stroke();
