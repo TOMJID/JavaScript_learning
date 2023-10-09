51bc5dbd4698578579ba3846e566c0fbfe0dd200
TOMJID <94817887+TOMJID@users.noreply.github.com>
2023-10-09 10:50:14 +0600

    #

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-var c = document.querySelector("#myCanvas");
+var nums1 = [22, 31, 4, 5, 35, 26, 78];
+var newNum = nums1.filter(function (x) {
+  return x>10;
+});
 
-var ctx = c.getContext("2d");
-
-ctx.lineWidth = 3;
-ctx.strokeStyle = "white";
-ctx.strokeRect(10, 10, 360, 210);
-
-ctx.fillStyle = "green";
-ctx.fillRect(12, 12, 358, 208);
-
-var centerX = 358 / 2;
-var centerY = 208 / 2;
-
-ctx.beginPath();
-ctx.arc(centerX, centerY, 50, 0, 2 * Math.PI, false);
-ctx.fillStyle = "red";
-ctx.lineWidth = 1;
-ctx.fill();
-ctx.stroke();
+console.log(newNum);
